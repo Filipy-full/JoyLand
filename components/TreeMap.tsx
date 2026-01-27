db2aeba017870dadaa1cfa3158f484ab9fd50e6b
Filipy-full <filipyhenrique54@gmail.com>
2026-01-27 19:35:19 +0000

    feat: cria componente TreeMapClient para encapsular a lógica de renderização do mapa

--- components/TreeMap.tsx
+++ components/TreeMap.tsx
@@ -1,11 +1,5 @@
-"use client";
-import dynamic from "next/dynamic";
 import type { Tree } from "./TreeMapLeafletClient";
-
-const TreeMapLeafletClient = dynamic(() => import("./TreeMapLeafletClient"), {
-  ssr: false,
-});
-
 export default function TreeMap({ trees, onTreeSelect }: { trees: Tree[]; onTreeSelect?: (tree: Tree) => void }) {
-  return <TreeMapLeafletClient trees={trees} onTreeSelect={onTreeSelect} />;
+  return <TreeMapClient trees={trees} onTreeSelect={onTreeSelect} />;
 }
+import TreeMapClient from "./TreeMapClient";
---
+++ components/TreeMapClient.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type { Tree } from "./TreeMapLeafletClient";
+
+const TreeMapLeafletClient = dynamic(
+  () => import("./TreeMapLeafletClient"),
+  { ssr: false }
+);
+
+export default function TreeMapClient({
+  trees,
+  onTreeSelect,
+}: {
+  trees: Tree[];
+  onTreeSelect?: (tree: Tree) => void;
+}) {
+  return (
+    <TreeMapLeafletClient trees={trees} onTreeSelect={onTreeSelect} />
+  );
+}
